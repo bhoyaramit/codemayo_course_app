7862bd32ed3281beb86e7e26ddc1daefaa70cad2
Amit Bhoyar <89729194+bhoyaramit@users.noreply.github.com>
2023-02-01 12:48:39 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -18,20 +18,3 @@ export const sendEmail = async (to, subject, text) => {
       text,
     });
   };
-// exports.sendEmail = async (options) => {
-  
-//     const transporter = nodemailer.createTransport({
-//       host: "smtp.mailtrap.io",
-//       port: 2525,
-//       auth: {
-//         user: "756c1e6abc0968",
-//         pass: "b83d410f1cba08"
-//       }
-//     });
-  
-//         await transporter.sendMail({
-  //     to,
-  //     subject,
-  //     text,from :"bhoyaramit9404@gmail.com"
-  //   });
-  // }
